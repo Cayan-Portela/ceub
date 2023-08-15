407edbd4770bafd9d28a25c1858b0aa13d3614fd
cayanportela <cayanportela@hotmail.com>
2023-08-15 08:22:39 -0300

    simula modelo linear

--- otimizacao/aula_01.R
+++ otimizacao/aula_01.R
@@ -1,1 +1,19 @@
 library(dplyr)
+library(ggplot2)
+# Modelo:
+# y = b0 + b1*x + e
+# y = 50 + 11*x + e
+
+n = 100
+set.seed(1234)
+# variavel independente
+x <- rgamma(n = n, shape = 100, rate = 300) * 200
+# erro
+erro <- rnorm(n = n, mean = 0, sd = 25)
+
+b0 = 50; b1 = 11
+
+y <- b0 + b1*x + erro
+
+ggplot() +
+    geom_point(x = x, y = y)
