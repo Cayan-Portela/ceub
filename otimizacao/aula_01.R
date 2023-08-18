468a0484b693eceeecff4ed4c6812d92d14ad153
cayanportela <cayanportela@hotmail.com>
2023-08-18 09:32:12 -0300

    reg linear, betas na mao

--- otimizacao/aula_01.R
+++ otimizacao/aula_01.R
@@ -1,19 +1,49 @@
+# pacotes ----------------------------------------------------------------------
 library(dplyr)
-library(ggplot2)
-# Modelo:
-# y = b0 + b1*x + e
-# y = 50 + 11*x + e
+#library(ggplot2)
 
-n = 100
-set.seed(1234)
-# variavel independente
-x <- rgamma(n = n, shape = 100, rate = 300) * 200
-# erro
-erro <- rnorm(n = n, mean = 0, sd = 25)
+# lendo dados ------------------------------------------------------------------
+dados <- read.csv(
+    'https://raw.githubusercontent.com/Cayan-Portela/ceub/main/dados/mtcars.csv'
+    )
+n_ <- nrow(dados)
 
-b0 = 50; b1 = 11
 
-y <- b0 + b1*x + erro
+# usando funcao lm() -----------------------------------------------------------
+mod <- lm(mpg ~ wt, data=dados)
+mod$coefficients
 
-ggplot() +
-    geom_point(x = x, y = y)
+# cria matriz X ----------------------------------------------------------------
+matriz_x <- function(colunas, dados) {
+    col_1 <- matrix(1, nrow = n_)
+    col_var <-  as.matrix(dados[colunas])
+    X_mat <- cbind(col_1, col_var)
+
+    return(X_mat)
+}
+
+# definndo X e Y ---------------------------------------------------------------
+X_mat <- matriz_x(colunas = c("wt"), dados=dados)
+y <- dados$mpg
+
+# definindo X' e X'X -----------------------------------------------------------
+Xl <- t(X_mat)
+Xlx <- Xl %*% X_mat
+
+# betas estimados --------------------------------------------------------------
+betas_mat <- solve(Xlx) %*% Xl %*% y
+betas_mat
+
+# ------------------
+# Decomposicao QR
+# ------------------
+QR <- qr(X_mat)
+Q <- qr.Q(QR)
+R <- qr.R(QR)
+betas_qr <- backsolve(R, crossprod(Q, y))
+
+# comparando betas -------------------------------------------------------------
+mod$coefficients
+betas_mat
+betas_qr
+
